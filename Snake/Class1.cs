ea302b9ed62d48bf39d740f2ef24d205a9ea3e45
bahitov1998 <40343243+bahitov1998@users.noreply.github.com>
2020-10-28 18:16:53 +0400

    encapsulation

--- Snake/Class1.cs
+++ Snake/Class1.cs
@@ -6,16 +6,17 @@ namespace Snake
     { 
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            Point p1 = new Point(1,3,'*');
+             /*p1.x = 1;
+             p1.y = 3;
+             p1.sym = '*';*/
+             p1.Draw();
 
             Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+             p2.x = 4;
+             p2.y = 5;
+             p2.sym = '#';
+             p2.Draw();
 
             /*int x1 = 1;
             int y1 = 3;
