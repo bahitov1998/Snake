ea0420296e50b4abd01fda64db50cbeec85feb24
bahitov1998 <40343243+bahitov1998@users.noreply.github.com>
2020-10-28 13:29:07 +0400

    x

--- Snake/Class1.cs
+++ Snake/Class1.cs
@@ -6,7 +6,13 @@ namespace Snake
     { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+
             Console.ReadLine();
         }
     }
